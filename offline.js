b5996878777ec5ded66d831f64737b5ece0b9a57
guzelku <62844142+guzelku@users.noreply.github.com>
2023-11-04 15:12:17 +0500

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1699091532,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/фон.png",
+		"images/трава.png",
+		"images/дорога.png",
+		"images/разделитель-sheet0.png",
+		"images/Уборщик-sheet0.png",
+		"images/Уборщик-sheet1.png",
+		"images/машина-sheet0.png",
+		"images/машина-sheet1.png",
+		"images/ограждение-sheet0.png",
+		"images/корзина-sheet0.png",
+		"images/мусор-sheet0.png",
+		"images/Победа-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
